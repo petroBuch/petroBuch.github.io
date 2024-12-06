80b5d1ff02daf865e5d40dd7b4df87d3b0907645
Salomatin Petr <salopetr01@yandex.ru>
2024-12-06 17:43:48 +0300

    add

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const cart_button = document.getElementById('user_cart_button');
+const katalog_button = document.getElementById('katalog_button');
+const main_page_start = document.getElementById('main_page_start');
+
+function goToProductPage(){window.location = "Product.html"}
+function goToMainPage(){ window.location = "index.html"}
+
+cart_button.onclick = goToProductPage;
+katalog_button.onclick = goToMainPage;
+main_page_start.onclick = goToMainPage;
