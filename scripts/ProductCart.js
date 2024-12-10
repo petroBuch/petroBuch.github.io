eaa4ac8795d77e18b32f0df4863f61492091e52c
Salomatin Petr <salopetr01@yandex.ru>
2024-12-10 04:29:11 +0300

    add

--- scripts/ProductCart.js
+++ scripts/ProductCart.js
@@ -107,7 +107,6 @@ for (let i = 0; i < minus_button.length; i++) {
     };
 }
 
-
 for (let i = 0; i < clear_button.length; i++) {
     clear_button[i].onclick = () => {
         const savedCart = JSON.parse(localStorage.getItem("cart"));
